425ef546ab3e5156cc75cf176075ea36d9d633ec
Sam <cplsyx@gmail.com>
2023-01-15 19:03:29 +0000

    Add files via upload

---
+++ Firmware/Secrets.h
@@ -0,0 +1,7 @@
+// MQTT username and password
+
+const char* mqtt_username = "username";
+const char* mqtt_password = "password";
+
+// MQTT Broker, change as required
+const char* mqtt_server = "0.0.0.0";
