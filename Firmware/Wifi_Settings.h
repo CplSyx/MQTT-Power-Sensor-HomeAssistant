7e4f00e42a91b999e178f0925eafe237c2b11505
Sam <cplsyx@gmail.com>
2023-01-15 19:13:22 +0000

    Accidentally overwrite previous settings, so restoring

--- Firmware/Wifi_Settings.h
+++ Firmware/Wifi_Settings.h
@@ -6,7 +6,7 @@
 #ifdef Fixed_IP
 #define STATIC_IP
 #endif
-IPAddress ip(192, 168, 1, 199);
+IPAddress ip(192, 168, 1, 15);
 IPAddress gateway(192, 168, 1, 1);
 IPAddress subnet(255, 255, 255, 0);
 
